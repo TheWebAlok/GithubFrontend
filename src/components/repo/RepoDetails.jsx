0a76a64f1e73f4215963d6857a806e1c2e63b408
alokprakash7766 <your_email@domain.com>
2026-03-14 10:13:56 +0530

    repodetials

--- src/components/repo/RepoDetails.jsx
+++ src/components/repo/RepoDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import "./RepoDetails.css";
+import "./Repodetails.css";
 
 const RepoDetails = () => {
 
